a8d8aae365b086d5e17a68c2a824132109e5a4b3
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-04-16 09:38:11 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { animate, motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
-import me from "../assets/Snapchat-660778217.jpg";
+import me from "../assets/IMG20240406163455.jpg";
 
 const Home = () => {
   const projectCount = useRef(null);
